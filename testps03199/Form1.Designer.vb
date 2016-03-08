5338e0d12d1c3c506ef0fed127b0a995d27bc719
linhps03199 <chaulinhtest@gmail.com>
2016-03-08 13:59:55 +0700

    test

---
+++ testps03199/Form1.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmQLSP
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.dgvQL = New System.Windows.Forms.DataGridView()
+        Me.btnLuu = New System.Windows.Forms.Button()
+        Me.btnXoa = New System.Windows.Forms.Button()
+        Me.btnSua = New System.Windows.Forms.Button()
+        Me.btnThem = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDongia = New System.Windows.Forms.TextBox()
+        Me.txtSoluong = New System.Windows.Forms.TextBox()
+        Me.txtTenSP = New System.Windows.Forms.TextBox()
+        Me.txtMaSP = New System.Windows.Forms.TextBox()
+        CType(Me.dgvQL, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Monotype Corsiva", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.LightSeaGreen
+        Me.Label5.Location = New System.Drawing.Point(181, -2)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(186, 25)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "Quản Lý Sản Phẩm"
+        '
+        'dgvQL
+        '
+        Me.dgvQL.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvQL.Location = New System.Drawing.Point(15, 104)
+        Me.dgvQL.Name = "dgvQL"
+        Me.dgvQL.Size = New System.Drawing.Size(554, 183)
+        Me.dgvQL.TabIndex = 22
+        '
+        'btnLuu
+        '
+        Me.btnLuu.Location = New System.Drawing.Point(491, 61)
+        Me.btnLuu.Name = "btnLuu"
+        Me.btnLuu.Size = New System.Drawing.Size(75, 23)
+        Me.btnLuu.TabIndex = 21
+        Me.btnLuu.Text = "Lưu"
+        Me.btnLuu.UseVisualStyleBackColor = True
+        '
+        'btnXoa
+        '
+        Me.btnXoa.Location = New System.Drawing.Point(491, 32)
+        Me.btnXoa.Name = "btnXoa"
+        Me.btnXoa.Size = New System.Drawing.Size(75, 23)
+        Me.btnXoa.TabIndex = 19
+        Me.btnXoa.Text = "Xóa"
+        Me.btnXoa.UseVisualStyleBackColor = True
+        '
+        'btnSua
+        '
+        Me.btnSua.Location = New System.Drawing.Point(410, 61)
+        Me.btnSua.Name = "btnSua"
+        Me.btnSua.Size = New System.Drawing.Size(75, 23)
+        Me.btnSua.TabIndex = 20
+        Me.btnSua.Text = "Sửa"
+        Me.btnSua.UseVisualStyleBackColor = True
+        '
+        'btnThem
+        '
+        Me.btnThem.Location = New System.Drawing.Point(410, 32)
+        Me.btnThem.Name = "btnThem"
+        Me.btnThem.Size = New System.Drawing.Size(75, 23)
+        Me.btnThem.TabIndex = 18
+        Me.btnThem.Text = "Thêm"
+        Me.btnThem.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(235, 71)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Đơn giá:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Tên SP:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(229, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Số lượng:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Mã SP:"
+        '
+        'txtDongia
+        '
+        Me.txtDongia.Location = New System.Drawing.Point(287, 64)
+        Me.txtDongia.Name = "txtDongia"
+        Me.txtDongia.Size = New System.Drawing.Size(100, 20)
+        Me.txtDongia.TabIndex = 16
+        '
+        'txtSoluong
+        '
+        Me.txtSoluong.Location = New System.Drawing.Point(287, 35)
+        Me.txtSoluong.Name = "txtSoluong"
+        Me.txtSoluong.Size = New System.Drawing.Size(100, 20)
+        Me.txtSoluong.TabIndex = 15
+        '
+        'txtTenSP
+        '
+        Me.txtTenSP.Location = New System.Drawing.Point(80, 68)
+        Me.txtTenSP.Name = "txtTenSP"
+        Me.txtTenSP.Size = New System.Drawing.Size(100, 20)
+        Me.txtTenSP.TabIndex = 14
+        '
+        'txtMaSP
+        '
+        Me.txtMaSP.Location = New System.Drawing.Point(80, 39)
+        Me.txtMaSP.Name = "txtMaSP"
+        Me.txtMaSP.Size = New System.Drawing.Size(100, 20)
+        Me.txtMaSP.TabIndex = 9
+        '
+        'frmQLSP
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(585, 304)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.dgvQL)
+        Me.Controls.Add(Me.btnLuu)
+        Me.Controls.Add(Me.btnXoa)
+        Me.Controls.Add(Me.btnSua)
+        Me.Controls.Add(Me.btnThem)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDongia)
+        Me.Controls.Add(Me.txtSoluong)
+        Me.Controls.Add(Me.txtTenSP)
+        Me.Controls.Add(Me.txtMaSP)
+        Me.Name = "frmQLSP"
+        Me.Text = "QUAN LY SAN PHAM"
+        CType(Me.dgvQL, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents dgvQL As System.Windows.Forms.DataGridView
+    Friend WithEvents btnLuu As System.Windows.Forms.Button
+    Friend WithEvents btnXoa As System.Windows.Forms.Button
+    Friend WithEvents btnSua As System.Windows.Forms.Button
+    Friend WithEvents btnThem As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtDongia As System.Windows.Forms.TextBox
+    Friend WithEvents txtSoluong As System.Windows.Forms.TextBox
+    Friend WithEvents txtTenSP As System.Windows.Forms.TextBox
+    Friend WithEvents txtMaSP As System.Windows.Forms.TextBox
+
+End Class
